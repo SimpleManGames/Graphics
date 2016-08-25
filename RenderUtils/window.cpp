ee68f510a7b65997b59289f11e2e04c4078a08a5
Riley Smith <riley.smith@sea.aie.edu>
2016-08-25 16:10:30 -0700

    Error checking

--- RenderUtils/window.cpp
+++ RenderUtils/window.cpp
@@ -4,6 +4,20 @@
 #include "glfw\glfw3.h"
 #include <cstring>
 #include "window.h"
+#include <cstdio>
+#include <cstdlib>
+
+#ifdef _DEBUG
+void APIENTRY GL_errorCallback(GLenum source, GLenum type, 
+	GLenum id, GLenum severity, 
+	GLsizei length, const GLchar *message, 
+	const void *userParam) {
+
+	fprintf(stderr, "%S\n", message);
+
+	if (severity == GL_DEBUG_SEVERITY_HIGH) abort();
+}
+#endif
 
 int Window::Initialize(int a_width, int a_height, char * a_title)
 {
@@ -20,6 +34,14 @@ int Window::Initialize(int a_width, int a_height, char * a_title)
 	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 
 	isInit = true;
+
+#ifdef _DEBUG
+	glEnable(GL_DEBUG_OUTPUT);
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+
+	glDebugMessageCallback(GL_errorCallback, 0);
+	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, 0, true);
+#endif
 
 	return true;
 }
